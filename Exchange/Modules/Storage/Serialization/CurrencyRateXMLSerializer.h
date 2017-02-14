ee836404b7c4c9fab53cc09319b02a2d22c2ab43
memoto <kirauser@gmail.com>
2017-02-15 02:34:07 +0300

    - ADD: Currency rates serialization

---
+++ Exchange/Modules/Storage/Serialization/CurrencyRateXMLSerializer.h
@@ -0,0 +1,13 @@
+//
+//  CurrencyRateXMLSerializer.h
+//  Exchange
+//
+//  Created by Константин on 15.02.17.
+//  Copyright © 2017 KYurichev. All rights reserved.
+//
+
+#import <AFNetworking/AFNetworking.h>
+
+@interface CurrencyRateXMLSerializer : AFXMLParserResponseSerializer
+
+@end
---
+++ Exchange/Modules/Storage/Serialization/CurrencyRateXMLSerializer.m
@@ -0,0 +1,13 @@
+//
+//  CurrencyRateXMLSerializer.m
+//  Exchange
+//
+//  Created by Константин on 15.02.17.
+//  Copyright © 2017 KYurichev. All rights reserved.
+//
+
+#import "CurrencyRateXMLSerializer.h"
+
+@implementation CurrencyRateXMLSerializer
+
+@end
